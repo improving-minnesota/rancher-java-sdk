e98423453a9f70d07b675fb8c65268ab297b0a0a
Thomas Rossignol <tro@zebaz.com>
2016-06-25 15:31:21 +0200

    Add method to get containers by service

--- src/main/java/io/rancher/service/ServiceService.java
+++ src/main/java/io/rancher/service/ServiceService.java
@@ -2,12 +2,12 @@ package io.rancher.service;
 
 import io.rancher.base.Filters;
 import io.rancher.base.TypeCollection;
-import io.rancher.type.Service;
-import io.rancher.type.SetServiceLinksInput;
-import io.rancher.type.ServiceUpgrade;
-import io.rancher.type.ServiceRestart;
 import io.rancher.type.AddRemoveServiceLinkInput;
-
+import io.rancher.type.Instance;
+import io.rancher.type.Service;
+import io.rancher.type.ServiceRestart;
+import io.rancher.type.ServiceUpgrade;
+import io.rancher.type.SetServiceLinksInput;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.http.Body;
@@ -29,6 +29,9 @@ public interface ServiceService {
   @GET("service/{id}")
   Call<Service> get(@Path("id") String id);
 
+  @GET("service/{id}/instances")
+  Call<TypeCollection<Instance>> listInstances(@Path("id") String id);
+
   @POST("service")
   Call<Service> create(@Body Service service);
 
@@ -37,41 +40,41 @@ public interface ServiceService {
 
   @DELETE("service/{id}")
   Call<Response> delete(@Path("id") String id);
-  
+
   @POST("service/{id}?action=activate")
   Call<Service> activate(@Path("id") String id);
-  
+
   @POST("service/{id}?action=addservicelink")
   Call<Service> addservicelink(@Path("id") String id, @Body AddRemoveServiceLinkInput addRemoveServiceLinkInput);
-  
+
   @POST("service/{id}?action=cancelrollback")
   Call<Service> cancelrollback(@Path("id") String id);
-  
+
   @POST("service/{id}?action=cancelupgrade")
   Call<Service> cancelupgrade(@Path("id") String id);
-  
+
   @POST("service/{id}?action=deactivate")
   Call<Service> deactivate(@Path("id") String id);
-  
+
   @POST("service/{id}?action=finishupgrade")
   Call<Service> finishupgrade(@Path("id") String id);
-  
+
   @POST("service/{id}?action=remove")
   Call<Service> remove(@Path("id") String id);
-  
+
   @POST("service/{id}?action=removeservicelink")
   Call<Service> removeservicelink(@Path("id") String id, @Body AddRemoveServiceLinkInput addRemoveServiceLinkInput);
-  
+
   @POST("service/{id}?action=restart")
   Call<Service> restart(@Path("id") String id, @Body ServiceRestart serviceRestart);
-  
+
   @POST("service/{id}?action=rollback")
   Call<Service> rollback(@Path("id") String id);
-  
+
   @POST("service/{id}?action=setservicelinks")
   Call<Service> setservicelinks(@Path("id") String id, @Body SetServiceLinksInput setServiceLinksInput);
-  
+
   @POST("service/{id}?action=upgrade")
   Call<Service> upgrade(@Path("id") String id, @Body ServiceUpgrade serviceUpgrade);
-  
+
 }
