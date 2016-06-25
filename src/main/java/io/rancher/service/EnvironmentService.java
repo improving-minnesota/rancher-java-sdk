86647df86118a1482ca7198ba11dd23c3645d109
Thomas Rossignol <tro@zebaz.com>
2016-06-25 15:29:46 +0200

    Add method to get services by environment

--- src/main/java/io/rancher/service/EnvironmentService.java
+++ src/main/java/io/rancher/service/EnvironmentService.java
@@ -2,12 +2,12 @@ package io.rancher.service;
 
 import io.rancher.base.Filters;
 import io.rancher.base.TypeCollection;
-import io.rancher.type.Environment;
 import io.rancher.type.AddOutputsInput;
-import io.rancher.type.ComposeConfigInput;
 import io.rancher.type.ComposeConfig;
+import io.rancher.type.ComposeConfigInput;
+import io.rancher.type.Environment;
 import io.rancher.type.EnvironmentUpgrade;
-
+import io.rancher.type.Service;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.http.Body;
@@ -26,6 +26,9 @@ public interface EnvironmentService {
   @GET("environment")
   Call<TypeCollection<Environment>> list(@QueryMap Filters filters);
 
+  @GET("environment/{id}/services")
+  Call<TypeCollection<Service>> listServices(@Path("id") String id);
+
   @GET("environment/{id}")
   Call<Environment> get(@Path("id") String id);
 
@@ -37,38 +40,38 @@ public interface EnvironmentService {
 
   @DELETE("environment/{id}")
   Call<Response> delete(@Path("id") String id);
-  
+
   @POST("environment/{id}?action=activateservices")
   Call<Environment> activateservices(@Path("id") String id);
-  
+
   @POST("environment/{id}?action=addoutputs")
   Call<Environment> addoutputs(@Path("id") String id, @Body AddOutputsInput addOutputsInput);
-  
+
   @POST("environment/{id}?action=cancelrollback")
   Call<Environment> cancelrollback(@Path("id") String id);
-  
+
   @POST("environment/{id}?action=cancelupgrade")
   Call<Environment> cancelupgrade(@Path("id") String id);
-  
+
   @POST("environment/{id}?action=deactivateservices")
   Call<Environment> deactivateservices(@Path("id") String id);
-  
+
   @POST("environment/{id}?action=error")
   Call<Environment> error(@Path("id") String id);
-  
+
   @POST("environment/{id}?action=exportconfig")
   Call<ComposeConfig> exportconfig(@Path("id") String id, @Body ComposeConfigInput composeConfigInput);
-  
+
   @POST("environment/{id}?action=finishupgrade")
   Call<Environment> finishupgrade(@Path("id") String id);
-  
+
   @POST("environment/{id}?action=remove")
   Call<Environment> remove(@Path("id") String id);
-  
+
   @POST("environment/{id}?action=rollback")
   Call<Environment> rollback(@Path("id") String id);
-  
+
   @POST("environment/{id}?action=upgrade")
   Call<Environment> upgrade(@Path("id") String id, @Body EnvironmentUpgrade environmentUpgrade);
-  
+
 }
